ff55bf2cf98604da620fc47129ba4b697be2f80d
Xecozz <94707887+Xecozz@users.noreply.github.com>
2023-01-15 01:16:56 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -19,12 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%1l#x*$hq155j=u36h3w^4#p$*)v!756qllekc-b_#p15s33=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 AUTHENTICATION_BACKENDS = [
     'discordAuth.auth.DiscordAuthentificationBackend',
